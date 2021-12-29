56ea2883ebf9eed440b677b10d403f5d7007fba0
Estefany Diaz <estefanydiaz75@gmail.com>
2021-12-28 20:57:27 -0500

    version inicial

--- main.js
+++ main.js
@@ -1,55 +1,56 @@
-const express = require("express"); 
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const router = express.Router();
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 // Soporte para bodies codificados en jsonsupport.
 app.use(bodyParser.json());
 // Soporte para bodies codificados
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const emailForSends = process.env.email;
 const password = process.env.password;
 
+console.log(emailForSends);
+console.log(password);
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: emailForSends,
-        pass: password
-    }
+  service: "gmail",
+  auth: {
+    user: emailForSends,
+    pass: password,
+  },
 });
 
 let mailOptions = {
-    from: emailForSends,
-    to: emailForSends,
-    subject: 'Nuevo Contacto!',
-    text: ''
-}
+  from: emailForSends,
+  to: emailForSends,
+  subject: "Nuevo Contacto!",
+  text: "",
+};
 
-router.post('/sendEmail', (request, response) => {
-    console.log(request.body);
-    const email = request.body.email;
-    const name =  request.body.name;
-    const message =  request.body.message;
-    
-    mailOptions["text"] = `Mensaje: ${message} 
+router.post("/sendEmail", (request, response) => {
+  console.log(request.body);
+  const email = request.body.email;
+  const name = request.body.name;
+  const message = request.body.message;
+
+  mailOptions["text"] = `Mensaje: ${message} 
 Nombre: ${name}
 Email: ${email}`;
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            response.status(500);
-            console.log(error);
-        } else {
-            response.end();
-        }
-    });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      response.status(500);
+      console.log(error);
+    } else {
+      response.end();
+    }
+  });
 });
-    
 
 app.use("/", router);
 
 app.listen(process.env.PORT, () => {
-    console.log("Started on PORT "+process.env.PORT);
-})
+  console.log("Started on PORT " + process.env.PORT);
+});
